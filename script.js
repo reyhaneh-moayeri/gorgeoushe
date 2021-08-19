60c595f0405c9176197c62634fb5a7108c3bb568
reyhaneh <78968279+reyhaneh-moayeri@users.noreply.github.com>
2021-08-19 15:31:17 +0430

    adding animation

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ textwrap.innerHTML = textwrap.textContent.replace(
 );
 console.log(textwrap);
 
-anime.timeline().add({
+const textanim = anime.timeline().add({
   targets: ".ml12 .letter",
   translateY: [100, 0],
   translateZ: 0,
@@ -14,3 +14,39 @@ anime.timeline().add({
   duration: 2000,
   delay: (el, i) => 2000 + 60 * i,
 });
+
+TweenMax.from(".left", 3, {
+  left: "-50%",
+  ease: Expo.easeInOut,
+  delay: 3.4,
+});
+
+TweenMax.from(".header h1", 3, {
+  left: "-100%",
+  ease: Expo.easeInOut,
+  delay: 3.4,
+});
+
+TweenMax.staggerFrom(
+  ".img",
+  1,
+  {
+    y: 60,
+    opacity: 0,
+    ease: Power2.easeOut,
+    delay: 6,
+  },
+  0.2
+);
+
+TweenMax.staggerFrom(
+  ".header p",
+  1,
+  {
+    y: 60,
+    opacity: 0,
+    ease: Power2.easeOut,
+    delay: 5.6,
+  },
+  0.2
+);
